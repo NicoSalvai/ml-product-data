b9589defa1155eec6104dbbeb690419926b87039
NicoSalvai <nicolas-salvai@hotmail.com>
2023-03-17 18:08:16 -0300

    Procesing searches from dynamoDB directly

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import scrapper as Scrapper
 import persistence.dynamodb.products as Products
+import persistence.dynamodb.searches as Searches
 from dotenv import load_dotenv
 import os
 
-def process_complete_search(url):
-    search_results = Scrapper.process_search(url, True if os.environ["debug"] == "True" else False)
+def process_complete_search(search):
+    search_results = Scrapper.process_search(search['link'], True if os.environ["debug"] == "True" else False)
     for item in search_results:
+        item['search_id'] = search['id']
+        item['tags'] = search['tags']
+        
         if True if os.environ["save"] == "True" else False:
             Products.add_or_update_product(item)
         else:
             print(item)
 
+def process_searches_from_db():
+    searches = Searches.get_searches()
+    for search in searches:
+        process_complete_search(search)
+        Searches.update_search_enabled(search['id'], False)
+
 
 load_dotenv()
-process_complete_search(url=os.environ["url"])
+process_searches_from_db()
---
+++ persistence/dynamodb/searches.py
@@ -0,0 +1,24 @@
+from persistence.dynamodb.resource_connection import get_table
+from boto3.dynamodb.conditions import Attr
+
+def get_searches_table():
+    return get_table('searches')
+
+
+def get_searches(table=None):
+    table = table if table is not None else get_searches_table()
+    result = table.scan(FilterExpression=Attr('enabled').eq(True))
+    return result['Items']
+
+def update_search_enabled(id: str, enabled:bool, table=None):
+    table = table if table is not None else get_searches_table()
+    result = table.update_item(
+        Key={
+            'id': id
+        },
+        UpdateExpression='SET enabled = :val1',
+        ExpressionAttributeValues={
+            ':val1': enabled
+        }
+    )
+    print(result)
